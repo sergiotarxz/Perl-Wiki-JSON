0100c379668d7d08bc20c004a77ec41a3a0486c6
Sergiotarxz <sergiotarxz@posteo.net>
2025-08-11 09:39:20 +0200

    Adding some pretty docs.

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# Perl-Wiki-JSON
-Generates a data structure representing a wiki article
+# NAME
+
+Wiki::JSON - Parse wiki-like articles to a data-structure transformable to JSON.
+
+# SYNOPSIS
+
+    use Wiki::JSON;
+
+    my $structure = Wiki::JSON->new->parse(<<'EOF');
+    = This is a wiki title
+    '''This is bold'''
+    ''This is italic''
+    '''''This is bold and italic'''''
+    == This is a smaller title, the user can use no more than 6 equal signs
+    <nowiki>''This is printed without expanding the special characters</nowiki>
+    * This
+    * Is
+    * A
+    * Bullet
+    * Point
+    * List
+    {{foo|Templates are generated|with their arguments}}
+    {{stub|This is under heavy development}}
+    The parser has some quirks == This will generate a title ==
+    ''' == '' Unterminated syntaxes will still be parsed until the end of file
+    EOF
+
+# DESCRIPTION
+
+A parser for a subset of a mediawiki-like syntax, quirks include some
+supposedly inline elements are parsed multi-line like headers, templates\*,
+italic and bolds.
+
+Lists are only one level and not everything in mediawiki is supported by the
+moment.
+
+# INSTANCE\_METHODS
+
+## new
+
+    my $wiki_parser = Wiki::JSON->new;
+
+# METHODS
+
+## parse
+
+    my $structure = $wiki_parser->parse($wiki_string);
+
+Parses the wiki format into a serializable to JSON or YAML Perl data structure.
+
+# BUGS
+
+The author thinks it is possible the parser hanging forever, use it in
+a subprocess the program can kill if it takes too long.
+
+The developer can use fork, waitpid, pipe, and non-blocking IO for that.
+
+# LEGAL
+
+Copyright ©Sergiotarxz (2025)
+
+You can use this software under the terms of the GPLv3 license or a new later
+version provided by the FSF or the GNU project.
+
+# SEE ALSO
+
+Look what is supported and how in the tests: [https://github.com/sergiotarxz/Perl-Wiki-JSON/tree/main/t](https://github.com/sergiotarxz/Perl-Wiki-JSON/tree/main/t)
--- lib/Wiki/JSON.pm
+++ lib/Wiki/JSON.pm
@@ -646,3 +646,75 @@ sub _try_parse_header( $self, $output, $wiki_text, $buffer, $i, $options ) {
     return ( 1, $i, $buffer );
 }
 1;
+
+=encoding utf8
+
+=head1 NAME
+
+Wiki::JSON - Parse wiki-like articles to a data-structure transformable to JSON.
+
+=head1 SYNOPSIS
+
+    use Wiki::JSON;
+
+    my $structure = Wiki::JSON->new->parse(<<'EOF');
+    = This is a wiki title
+    '''This is bold'''
+    ''This is italic''
+    '''''This is bold and italic'''''
+    == This is a smaller title, the user can use no more than 6 equal signs
+    <nowiki>''This is printed without expanding the special characters</nowiki>
+    * This
+    * Is
+    * A
+    * Bullet
+    * Point
+    * List
+    {{foo|Templates are generated|with their arguments}}
+    {{stub|This is under heavy development}}
+    The parser has some quirks == This will generate a title ==
+    ''' == '' Unterminated syntaxes will still be parsed until the end of file
+    EOF
+
+=head1 DESCRIPTION
+
+A parser for a subset of a mediawiki-like syntax, quirks include some
+supposedly inline elements are parsed multi-line like headers, templates*,
+italic and bolds.
+
+Lists are only one level and not everything in mediawiki is supported by the
+moment.
+
+=head1 INSTANCE_METHODS
+
+=head2 new
+
+    my $wiki_parser = Wiki::JSON->new;
+
+=head1 METHODS
+
+=head2 parse
+
+    my $structure = $wiki_parser->parse($wiki_string);
+
+Parses the wiki format into a serializable to JSON or YAML Perl data structure.
+
+=head1 BUGS
+
+The author thinks it is possible the parser hanging forever, use it in
+a subprocess the program can kill if it takes too long.
+
+The developer can use fork, waitpid, pipe, and non-blocking IO for that.
+
+=head1 LEGAL
+
+Copyright ©Sergiotarxz (2025)
+
+You can use this software under the terms of the GPLv3 license or a new later
+version provided by the FSF or the GNU project.
+
+=head1 SEE ALSO
+
+Look what is supported and how in the tests: L<https://github.com/sergiotarxz/Perl-Wiki-JSON/tree/main/t>
+
+=cut
