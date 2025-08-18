e88735ced5a35f6548267a0726fe702495d3f995
Sergiotarxz <sergiotarxz@posteo.net>
2025-08-18 04:06:26 +0200

    Adding missing file for 0.0.16

---
+++ check_before_version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+error() {
+    echo Fase failed
+    exit 1;
+}
+
+echo "Testing current perl";
+perlbrew exec prove || error
+echo "Testing 5.16.3";
+perlbrew exec --with perl-5.16.3 prove || error
+perl -e '
+    use v5.38.2;
+    use Path::Tiny;
+    say "Checking version in Changelog";
+    my ($version) = path("lib/Wiki/JSON.pm")->slurp_utf8 =~ /\$VERSION\s+=\s+"(.*?)"/;
+    say "version=$version";
+    if (path("Changes")->slurp_utf8 !~ /$version/) {
+        exit 1;
+    }
+    say "Version $version Present";
+' || error
+pod2markdown < lib/Wiki/JSON.pm > README.md || error
+perl Build.PL
+./Build dist
